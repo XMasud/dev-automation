8e8bd115098cdff26e2f2f91cbc150fb091e549a
Masud Rana <masud.aiub.cse@gmail.com>
2025-05-16 19:14:39 +0200

    dev automation project added

---
+++ src/main/java/com/pm/devautomation/DevAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.pm.devautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/devautomation/DevAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.devautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
